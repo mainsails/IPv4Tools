c883bcf519d633e6ada54273c2b1fac78cbba983
Shaw Sam <Sam.Shaw@berkshire.nhs.uk>
2019-01-10 16:24:20 +0000

    Update comment-based help :: Minor cleanup

--- Public/Invoke-IPv4Scan.ps1
+++ Public/Invoke-IPv4Scan.ps1
@@ -3,20 +3,69 @@ Function Invoke-IPv4Scan {
     .SYNOPSIS
         Asynchronous IPv4 Network Scanner
     .DESCRIPTION
-        This asynchronous IPv4 Network Scanner allows you to scan an IPv4-Range.
-        The default result will contain the the IPv4-Address, Status (Up or Down) and the Hostname.
+        This asynchronous IPv4 Network Scanner allows you to scan an IPv4 Range.
+        The default result will contain the the IPv4 Address, Status (Up or Down), Hostname and MAC Address.
+    .PARAMETER StartIPv4Address
+        Starting IP Address.
+    .PARAMETER EndIPv4Address
+        Ending IP Address.
+    .PARAMETER IPv4Address
+        The IPv4 adress to use alongside either the 'Netmask' or 'CIDR' parameters for IP range input.
+    .PARAMETER Netmask
+        The netmask address to use alongside the 'IPv4Address' parameter for IP range input.
+    .PARAMETER CIDR
+        The CIDR prefix to use alongside the 'IPv4Address' parameter for IP range input.
+    .PARAMETER DisableDNSResolution
+        This is an optional parameter switch that disables DNS resolution during the scan.
+    .PARAMETER DisableMACResolution
+        This is an optional parameter switch that disables MAC address resolution during the scan.
+    .PARAMETER ExtendedInformation
+        This is an optional parameter switch that includes buffer size, response time and TTL in the output.
+    .PARAMETER IncludeInactive
+        This is an optional parameter switch that includes addresses that don't respond (all) in the output.
+    .PARAMETER Threads
+        This is an optional parameter that sets the number of threads to run concurrently.
+    .PARAMETER Count
+        This is an optional parameter that sets the number of echo requests to send.
     .EXAMPLE
-        Invoke-IPv4Scan -StartIPv4Address 10.210.86.0 -EndIPv4Address 10.210.86.255
+        Invoke-IPv4Scan -StartIPv4Address 192.168.0.1 -EndIPv4Address 192.168.0.255
+
+        IPv4Address   Status MAC               Hostname
+        -----------   ------ ---               --------
+        192.168.0.25  Up     36-8A-2C-02-93-D4 Computer1.Domain.com
+
+    .EXAMPLE
+        Invoke-IPv4Scan -IPv4Address 192.168.0.1 -CIDR 24 -DisableDNSResolution
+
+        IPv4Address   Status MAC
+        -----------   ------ ---
+        192.168.0.25  Up     36-8A-2C-02-93-D4
+
+    .EXAMPLE
+        Invoke-IPv4Scan -IPv4Address 192.168.0.1 -CIDR 24 -DisableMACResolution
 
         IPv4Address   Status Hostname
-        -----------   ------ --------
-        10.210.86.22  Up     rwx52014.berkshire.nhs.uk
-    .EXAMPLE
-        Invoke-IPv4Scan -StartIPv4Address 10.210.86.0 -EndIPv4Address 10.210.86.255 -DisableDNSResolving
+        -----------   ------ ---
+        192.168.0.25  Up     Computer1.Domain.com
 
-        IPv4Address   Status
-        -----------   ------
-        10.210.86.1   Up
+    .EXAMPLE
+        Invoke-IPv4Scan -IPv4Address 192.168.0.1 -CIDR 24 -IncludeInactive -ExtendedInformation
+
+        IPv4Address  : 192.168.0.1
+        Status       : Down
+        MAC          :
+        Hostname     :
+        BufferSize   :
+        ResponseTime :
+        TTL          :
+
+        IPv4Address  : 192.168.0.2
+        Status       : Up
+        MAC          : 92-A0-B5-67-89-97
+        Hostname     : Computer2.Domain.com
+        BufferSize   : 32
+        ResponseTime : 4
+        TTL          : 128
     #>
 
     [CmdletBinding(DefaultParameterSetName='CIDR')]
@@ -35,19 +84,19 @@ Function Invoke-IPv4Scan {
         [Parameter(ParameterSetName='Address')]
         [ValidateRange(0,32)]
         [Int32]$CIDR,
-        [Int32]$Tries = 2,
-        [Int32]$Threads = 256,
-        [Switch]$DisableDNSResolving,
-        [Switch]$DisableMACResolving,
+        [Switch]$DisableDNSResolution,
+        [Switch]$DisableMACResolution,
         [Switch]$ExtendedInformation,
-        [Switch]$IncludeInactive
+        [Switch]$IncludeInactive,
+        [Int32]$Threads = 256,
+        [Int32]$Count = 2
     )
 
     Begin {}
     Process {
         # Calculate IP range
         If ($IPv4Address -and $Netmask) { $Mask = Convert-SubnetMask -Netmask $Netmask }
-        If ($IPv4Address -and $CIDR)    { $Mask = Convert-SubnetMask -Netmask $CIDR }
+        If ($IPv4Address -and $CIDR)    { $Mask = Convert-SubnetMask -CIDR $CIDR }
         If ($IPv4Address -and $Mask) {
             $IP = Get-IPv4Calculation -IPv4Address $IPv4Address -Netmask $($Mask.Netmask)
             $StartIPv4Address = $IP.HostMin
@@ -59,10 +108,10 @@ Function Invoke-IPv4Scan {
         # Set output properties
         $PropertiesToDisplay = @()
         $PropertiesToDisplay += 'IPv4Address','Status'
-        If ($DisableMACResolving -eq $false) {
+        If ($DisableMACResolution -eq $false) {
             $PropertiesToDisplay += 'MAC'
         }
-        If ($DisableDNSResolving -eq $false) {
+        If ($DisableDNSResolution -eq $false) {
             $PropertiesToDisplay += 'Hostname'
         }
         If ($ExtendedInformation) {
@@ -73,16 +122,16 @@ Function Invoke-IPv4Scan {
         [System.Management.Automation.ScriptBlock]$ScriptBlock = {
             Param (
                 $IPv4Address,
-                $Tries,
-                $DisableDNSResolving,
-                $DisableMACResolving,
+                $Count,
+                $DisableDNSResolution,
+                $DisableMACResolution,
                 $ExtendedInformation,
                 $IncludeInactive
             )
 
             # ICMP
             $Status = [String]::Empty
-            For ($i = 0; $i -lt $Tries; i++) {
+            For ($i = 0; $i -lt $Count; i++) {
                 Try {
                     $PingObj    = New-Object -TypeName System.Net.NetworkInformation.Ping
                     $Timeout    = 1000
@@ -118,7 +167,7 @@ Function Invoke-IPv4Scan {
 
             # DNS
             $Hostname = [String]::Empty
-            If ((-not ($DisableDNSResolving)) -and (($Status -eq 'Up') -or $IncludeInactive)) {
+            If ((-not ($DisableDNSResolution)) -and (($Status -eq 'Up') -or $IncludeInactive)) {
                 Try {
                     $Hostname = ([System.Net.Dns]::GetHostEntry($IPv4Address).HostName)
                 }
@@ -127,7 +176,7 @@ Function Invoke-IPv4Scan {
 
             # MAC
             $MAC = [String]::Empty
-            If ((-not ($DisableMACResolving)) -and (($Status -eq 'Up') -or $IncludeInactive)) {
+            If ((-not ($DisableMACResolution)) -and (($Status -eq 'Up') -or $IncludeInactive)) {
                 $Arp_Result = (& ARP -a).ToUpper()
                 Foreach ($Line in $Arp_Result) {
                     If ($Line.TrimStart().StartsWith($IPv4Address)) {
@@ -166,10 +215,10 @@ Function Invoke-IPv4Scan {
             # Create parameter hashtable
             $ScriptParams = @{
                 IPv4Address          = $IPv4Address
-                Tries                = $Tries
-                DisableDNSResolving  = $DisableDNSResolving
-                DisableMACResolving  = $DisableMACResolving
-                ExtendedInformations = $ExtendedInformation
+                Count                = $Count
+                DisableDNSResolution = $DisableDNSResolution
+                DisableMACResolution = $DisableMACResolution
+                ExtendedInformation  = $ExtendedInformation
                 IncludeInactive      = $IncludeInactive
             }
 
@@ -199,14 +248,14 @@ Function Invoke-IPv4Scan {
 
             # Process completed jobs
             ForEach ($Job in $Jobs_ToProcess) {
-                # Get the result...
+                # Get the result
                 $Job_Result = $Job.Pipe.EndInvoke($Job.Result)
                 $Job.Pipe.Dispose()
 
-                # Remove job from collection
+                # Remove job
                 $Jobs.Remove($Job)
 
-                # Check if result contains status
+                # Check result
                 If ($Job_Result.Status) {
                     $Job_Result | Select-Object -Property $PropertiesToDisplay
                 }
